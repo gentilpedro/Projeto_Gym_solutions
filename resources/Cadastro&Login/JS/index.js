8cc538203e4ea4fc9c0cb69285371253519df0e3
Pedro Gentil <114460216+gentilpedro@users.noreply.github.com>
2023-11-16 23:21:13 +0000

    Comiit--11

--- resources/Cadastro&Login/JS/index.js
+++ resources/Cadastro&Login/JS/index.js
@@ -1,15 +1,15 @@
-if (localStorage.getItem("token") == null) {
-    alert("Você precisa estar logado para acessar essa página");
-    window.location.href = "./assets/html/singin.html";
-  }
-  
-  const userLogado = JSON.parse(localStorage.getItem("userLogado"));
-  
-  const logado = document.querySelector("#logado");
-  logado.innerHTML = `Olá ${userLogado.nome}`;
-  
-  function sair() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("userLogado");
-    window.location.href = "./assets/html/signin.html";
-  }
+  document.getElementById("recuperaSenha").addEventListener("submit", function (event) {
+    event.preventDefault(); // Evitar o envio do formulário
+
+    var senha = document.getElementById("password").value; // Pegar o valor do campo de senha
+
+    if (senha !== "") {
+      // Se a senha estiver preenchida, mostrar o alerta
+      alert("Mensagem de alerta aqui!");
+      // Redirecionar de volta para index.html
+      window.location.href = "index.html";
+    } else {
+      // Se a senha não estiver preenchida, n fazer nada ou exibir outra mensagem de erro
+      alert("Preencha a senha!");
+    }
+  });
